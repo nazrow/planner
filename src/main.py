a11783ad0ae8e308e80782fa18ddff8dd0c4db19
nazrow <nazrow@me.com>
2025-01-02 05:50:12 +0300

    host net

--- src/main.py
+++ src/main.py
@@ -23,4 +23,4 @@ def download():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=80, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=11111, reload=True)
