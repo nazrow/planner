dfd2e04f45ebae0c6cc1f845680a1f29bf76743f
nazrow <nazrow@me.com>
2025-01-09 19:50:25 +0300

    bump

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 fetch('api').then(function(response) {
-  return response.json();
-}).then(function(tasks) {
-  console.log(tasks);
-  draw(tasks);
+    return response.json();
+}).then(function(data) {
+    var tasks = JSON.parse(data);
+    console.log(tasks);
+    draw(tasks);
 }).catch(function(err) {
-  console.log(err);
+    console.log(err);
 });
 
 function draw(tasks) {
-  tasks.forEach((task) => {
-      var node = document.createElement('div');
-      node.id = task.id;
-      node.style.top = `${task.startline_hours}px`;
-      node.style.height = `${task.estimate * 8}px`;
-      node.style.left = `${task.lane * 260}px`;
-      node.textContent = task.description;
-      document.body.appendChild(node);
-  });
+    tasks.forEach((task) => {
+        var node = document.createElement('div');
+        node.id = task.id;
+        node.style.top = `${task.startline_hours}px`;
+        node.style.height = `${task.estimate * 8}px`;
+        node.style.left = `${task.lane * 260}px`;
+        node.textContent = task.description;
+        document.body.appendChild(node);
+    });
 }
