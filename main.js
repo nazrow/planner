390277b243d24f360ac04c4b7d7b86ce27331649
nazrow <nazrow@me.com>
2025-01-09 19:35:29 +0300

    no heavy lifting on backend

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ fetch('api').then(function(response) {
   return response.json();
 }).then(function(tasks) {
   console.log(tasks);
+  draw(tasks);
+}).catch(function(err) {
+  console.log(err);
+});
+
+function draw(tasks) {
   tasks.forEach((task) => {
       var node = document.createElement('div');
       node.id = task.id;
@@ -11,6 +17,4 @@ fetch('api').then(function(response) {
       node.textContent = task.description;
       document.body.appendChild(node);
   });
-}).catch(function(err) {
-  console.log(err);
-});
+}
