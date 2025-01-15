3c2fde6576c5d0661bf977765c16c9d7f0a816cd
Vlad Savenkov <vmsavenkov@sberbank.ru>
2025-01-15 15:17:06 +0300

    debug

--- www/main.js
+++ www/main.js
@@ -40,16 +40,19 @@ function draw(tasks) {
             task.node.style.top = `${task.y}px`;
             task.node.style.left = `${task.x}px`;
         });
-        main.style.marginTop = `${Math.min(tasks.map((task) => parseFloat(task.node.style.top))) * -1}px`;
-        main.style.marginBottom = `${Math.max(tasks.map((task) => parseFloat(task.node.style.top))) + 400}px`;
-        main.style.marginLeft = `${Math.min(tasks.map((task) => parseFloat(task.node.style.left))) * -1}px`;
-        main.style.marginRight = `${Math.max(tasks.map((task) => parseFloat(task.node.style.left))) + 250}px`;
+        var tops = tasks.map((task) => parseFloat(task.node.style.top));
+        var lefts = tasks.map((task) => parseFloat(task.node.style.left));
+        main.style.marginTop = `${Math.abs(Math.min(tops))}px`;
+        main.style.marginBottom = `${Math.abs(Math.max(tops)) + 400}px`;
+        main.style.marginLeft = `${Math.abs(Math.min(lefts))}px`;
+        main.style.marginRight = `${Math.abs(Math.max(lefts)) + 250}px`;
     }
     console.log(tasks[0].node);
     console.log(tasks[0].node.style);
     console.log(tasks[0].node.style.top);
     console.log(tasks[0].node.style.getPropertyValue('top'));
-    console.log(tasks.map((task) => parseFloat(task.node.style.top)));
-    console.log(Math.min(tasks.map((task) => parseFloat(task.node.style.top))));
+    var ttops = tasks.map((task) => parseFloat(task.node.style.top));
+    console.log(ttops);
+    console.log(Math.min(ttops));
     simulation.stop();
 }
