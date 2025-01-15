56166f51fe25a00e29e030251e374e0637949fb9
Vlad Savenkov <vmsavenkov@sberbank.ru>
2025-01-15 15:09:44 +0300

    debug

--- www/main.js
+++ www/main.js
@@ -40,15 +40,15 @@ function draw(tasks) {
             task.node.style.top = `${task.y}px`;
             task.node.style.left = `${task.x}px`;
         });
-//        document.getElementById('main').style.marginTop = `${Math.min(tasks.map((task) => task.node.style.getPropertyValue('top'))) * -1}px`;
-//        document.getElementById('main').style.marginBottom = `${Math.max(tasks.map((task) => task.node.style.getPropertyValue('top'))) + 400}px`;
-//        document.getElementById('main').style.marginLeft = `${Math.min(tasks.map((task) => task.node.style.getPropertyValue('left'))) * -1}px`;
-//        document.getElementById('main').style.marginRight = `${Math.max(tasks.map((task) => task.node.style.getPropertyValue('left'))) + 250}px`;
+        document.getElementById('main').style.marginTop = `${Math.min(tasks.map((task) => task.node.style.top)) * -1}px`;
+        document.getElementById('main').style.marginBottom = `${Math.max(tasks.map((task) => task.node.style.top)) + 400}px`;
+        document.getElementById('main').style.marginLeft = `${Math.min(tasks.map((task) => task.node.style.left)) * -1}px`;
+        document.getElementById('main').style.marginRight = `${Math.max(tasks.map((task) => task.node.style.left)) + 250}px`;
     }
     console.log(tasks[0].node);
     console.log(tasks[0].node.style);
     console.log(tasks[0].node.style.top);
     console.log(tasks[0].node.style.getPropertyValue('top'));
-    console.log(tasks.map((task) => task.node.style));
+    console.log(tasks.map((task) => task.node.style.top));
     simulation.stop();
 }
