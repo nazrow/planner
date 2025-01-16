87d39c0a84d4479906816db0fa126781a4b62038
Vlad Savenkov <vmsavenkov@sberbank.ru>
2025-01-16 15:27:53 +0300

    sparser scaling

--- www/main.js
+++ www/main.js
@@ -80,7 +80,7 @@ function draw(tasks) {
     var highest = Math.min(...tasks.map((task) => task.y));
     var leftest = Math.min(...tasks.map((task) => task.x));
     tasks.forEach((task) => {
-        task.node.style.top = `${(task.y - highest) / 100}em`;
-        task.node.style.left = `${(task.x - leftest) / 100}em`;
+        task.node.style.top = `${(task.y - highest) / 30}em`;
+        task.node.style.left = `${(task.x - leftest) / 30}em`;
     });
 }
