ed3fe02d804f8c7c5d793a17738ac7cb500dc0dc
Vlad Savenkov <vmsavenkov@sberbank.ru>
2025-01-16 14:59:43 +0300

    stronger collide

--- www/main.js
+++ www/main.js
@@ -62,7 +62,7 @@ function draw(tasks) {
         .force('doable', () => {
             tasks.forEach(task => {
                 if (!task.is_doable) {
-                    task.vy += 0.2;
+                    task.vy += 0.02;
                 }
             })
         })
@@ -75,7 +75,7 @@ function draw(tasks) {
             })
         })
         .force('charge', d3.forceManyBody())
-        .force('collide', d3.forceCollide((task) => Math.max(12, parseFloat(task.node.style.height))));
+        .force('collide', d3.forceCollide((task) => 2 * Math.max(12, parseFloat(task.node.style.height))));
     simulation.stop();
     while (simulation.alpha() >= simulation.alphaMin()) {
         simulation.tick();
