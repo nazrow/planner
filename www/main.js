352b61ff8304bc062e644edfa894b4a470b417c0
vmsavenkov <vmsavenkov@sberads.tech>
2025-02-13 17:52:41 +0300

    huh

--- www/main.js
+++ www/main.js
@@ -148,17 +148,16 @@ function draw(tasks) {
                 });
             }
         })
-        .force('collide', d3.forceManyBody().strength(-100).distanceMax(300))
-        .force('charge', d3.forceManyBody().strength(-10).distanceMin(150).distanceMax(2000))
+        .force('charge', d3.forceManyBody().strength(-20).distanceMax(2000))
         .force('gravity', d3.forceManyBody().strength(15).distanceMin(1800))
         .force('links', d3.forceLink(links).id((task) => task.id).distance(200))
-//        .force('collide', () => {
-//            if ((iteration + 1) % 20 == 0) {
-//                console.log('applying collision...')
-//                var collisions = findCollisions(tasks);
-//                solveCollisions(collisions, tasks);
-//            }
-//        })
+        .force('collide', () => {
+            if ((iteration + 1) % 20 == 0) {
+                console.log('applying collision...')
+                var collisions = findCollisions(tasks);
+                solveCollisions(collisions, tasks);
+            }
+        })
     simulation.stop();
 
     while (simulation.alpha() >= simulation.alphaMin()) {
