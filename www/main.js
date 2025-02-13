e55ede98b7249f7c82a2d1f7939b3a3dca2c8569
vmsavenkov <vmsavenkov@sberads.tech>
2025-02-13 18:15:23 +0300

    huh

--- www/main.js
+++ www/main.js
@@ -195,12 +195,12 @@ function draw(tasks) {
     links.forEach((link) => {
         var start = link.source;
         var end = link.target;
-        var [start_x, start_y] = [(start.left + start.right) / 2, start.bottom];
-        var [end_x, end_y] = [(end.left + end.right) / 2, end.top];
+        var [start_x, start_y] = [start.x + start.width / 2, start.y + start.height];
+        var [end_x, end_y] = [end.x + end.width / 2, end.y];
         console.log('drawing curve:', link, start, end, start_x, start_y, end_x, end_y);
         ctx.beginPath();
         ctx.moveTo(start_x, start_y);
-        ctx.bezierCurveTo(start_x, start_y + 50, end_x, end_y - 50, end_x, end_y);
+        ctx.bezierCurveTo(start_x, start_y + 250, end_x, end_y - 250, end_x, end_y);
         ctx.stroke();
     })
 }
