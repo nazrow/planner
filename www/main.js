364e596fbd93a94fe79ce3f57b2c3aa9028418ea
vmsavenkov <vmsavenkov@sberads.tech>
2025-02-13 16:49:37 +0300

    huh

--- www/main.js
+++ www/main.js
@@ -209,7 +209,7 @@ function draw(tasks) {
             }
         })
         .force('charge', d3.forceManyBody().strength(-10))
-        .force('links', d3.forceLink(links).id((task) => task.id).distance((link) => Math.hypot(link.source_task.x - link.target_task.x, link.source_task.y - link.target_task.y)).strength((link) => {link.distance() - 500 / 1500}));
+        .force('links', d3.forceLink(links).id((task) => task.id).distance((link) => {return Math.hypot(link.source_task.x - link.target_task.x, link.source_task.y - link.target_task.y)}).strength((link) => {return (link.distance() - 500) / 1500}));
     simulation.stop();
 //    console.log('right after creating simulation:', tasks);
 
