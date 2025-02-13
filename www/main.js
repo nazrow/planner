f425866a5671a046e7bf28224c5eaf17f52445fd
vmsavenkov <vmsavenkov@sberads.tech>
2025-02-13 18:04:49 +0300

    huh

--- www/main.js
+++ www/main.js
@@ -83,7 +83,7 @@ function draw(tasks) {
         var node = document.createElement('div');
         node.id = task.id;
         node.textContent = task.description;
-        node.style.minHeight = `${task.estimate * 10}px`;
+//        node.style.minHeight = `${task.estimate * 10}px`;
         node.classList.add('task');
         if (task.is_done) {
             node.classList.add('done');
